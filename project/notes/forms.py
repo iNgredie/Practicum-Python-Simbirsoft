e30e76a843edcf60960617a71ef4e80e826a3091
Aleksandr Durnev <sandslash@bk.ru>
2020-12-14 15:41:39 +0400

    black

--- project/notes/forms.py
+++ project/notes/forms.py
@@ -8,9 +8,36 @@ User = get_user_model()
 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(label="Email address", widget=forms.EmailInput(attrs={"class": "form-control", "name": "email", "placeholder": "Email address"}))
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={"class": "form-control", "name": "password", "placeholder": "Password"}))
-    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(attrs={"class": "form-control", "name": "password", "placeholder": "Confirm password"}))
+    email = forms.EmailField(
+        label="Email address",
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "name": "email",
+                "placeholder": "Email address",
+            }
+        ),
+    )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "name": "password",
+                "placeholder": "Password",
+            }
+        ),
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "name": "password",
+                "placeholder": "Confirm password",
+            }
+        ),
+    )
     username = None
 
     class Meta:
